8744cf9ccdadf35797937e10c602d2576539a130
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-06-13 16:38:49 -0700

    Create and initialize class attributes

--- src/Pathfinding/GreedyBFS/GreedyBFS.ts
+++ src/Pathfinding/GreedyBFS/GreedyBFS.ts
@@ -2,10 +2,25 @@ import { NodeType } from "../../App/Node";
 import {IGrid, INode, Pathfinder, PathInfo, SearchedNode} from "../Pathfinder";
 
 export class GreedyBFS implements Pathfinder {
+    grid: IGrid;
+    startNode: INode | undefined;
+    endNode: INode | undefined;
+    prioQueue: INode[] = [];
+
+    searchOrder: SearchedNode[];
+    shortestPath: number[];
+    pathFound: boolean;
     
-    
+
     constructor(height: number, width: number, nodes: INode[]) {
-                
+
+        this.grid = {
+            height, width, nodes
+        }
+
+        this.searchOrder = [];
+        this.shortestPath = [];
+        this.pathFound = false;
     }
 
 
