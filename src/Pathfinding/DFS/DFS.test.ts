d99c893d19f0edbb8d7b47c58974d18671820731
donggyu <donggyu258@gmail.com>
2022-06-13 16:18:08 -0700

    Make first test more thorough

--- src/Pathfinding/DFS/DFS.test.ts
+++ src/Pathfinding/DFS/DFS.test.ts
@@ -3,21 +3,22 @@ import { IGrid, Pathfinder, PathInfo } from '../Pathfinder';
 import { NodeType} from '../../App/Node';
 import {Util} from '../util.test';
 
-test('DFS1', () => {
+test('Ignore forest nodes', () => {
 
     let testGrid: IGrid = Util.createGrid(4, 7);
     testGrid.nodes[16].nodeType = NodeType.StartNode;
-    for (let i = 17; i < 20; i++) {
-        testGrid.nodes[i].nodeType = NodeType.ForestNode;
+    let path = [16, 9, 2, 3, 4, 5, 6, 13, 20];
+    for (const n of path) {
+        testGrid.nodes[n].nodeType = NodeType.ForestNode;
     }
     testGrid.nodes[20].nodeType = NodeType.EndNode;
 
     let algorithm: Pathfinder = new DFS(testGrid.height, testGrid.width, testGrid.nodes);
     const result: PathInfo = algorithm.findPath();
     expect(result.pathFound).toEqual(true);
-    expect(result.searchOrder.map(node => node.index)).toEqual([16, 9, 2, 3, 4, 5, 6, 13, 20]);
+    expect(result.searchOrder.map(node => node.index)).toEqual(path);
     expect(result.searchOrder.map(node => node.distance)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
-    expect(result.shortestPath).toEqual([16, 9, 2, 3, 4, 5, 6, 13, 20]);
+    expect(result.shortestPath).toEqual(path);
 });
 
 test("DFS2", () => {
